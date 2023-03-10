604f577ff26c0fecce7f0dccd99460e5baf64cdc
sneha039 <123440006+sneha039@users.noreply.github.com>
2023-03-10 14:21:20 +0530

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,30 @@
+# a=int(input("Enter the Value of a:"))
+# b=int(input("Enter the Value of b:"))
+# a,b=[int(x) for x in input().split()]
+# print(a,b)
+
+# a=int(input("Enter the Value of a:"))
+# if( a==5):
+#  print("Yes Equal")
+
+# a,b=[int(x) for x in input().split()]
+# if(a>b):
+#     print(f"{a}>{b} is {a>b}")
+# else:
+#   print(f"{a}<{b} is {a<b}")
+
+# a,b=[int(x) for x in input().split()]
+# if(a>b):print("a is greater than b")
+# if(a<b):print("a is less than b")
+# if(a<=b):print("a is less than  or equal to b")
+# if(a>=b):print("a is greater than or equal to b")
+# if(a!=b):print("a is not equal to b")
+# if(a==b):print("a is equal to or is same as b")
+
+# a,b=[int(x) for x in input().split()]
+# if(a>b):print("a is greater than b")
+# elif(a<b):print("a is less than b")
+# elif(a<=b):print("a is less than  or equal to b")
+# elif(a>=b):print("a is greater than or equal to b")
+# elif(a!=b):print("a is not equal to b")
+# elif(a==b):print("a is equal to or is same as b")
---
+++ if_statement_part2.py
@@ -0,0 +1,57 @@
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# if(a<b):
+#     print(f"{a} < {b} is True")
+
+
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# if(a<b):
+#     print(f"{a} < {b} is True")
+#     print("Hey Python")
+#     print("Its done.")
+
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# if(a<b):
+#     print(f"{a} < {b} is True")
+#     print("Hey Python")
+#     print("Its done.")
+# print("Try Next Time")
+
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# if(a<b):
+#     print(f"{a} < {b} is True")
+# if(a<=b):
+#     print(f"{a} < = {b} is True")
+# if(a>b):
+#     print(f"{a} > {b} is True")
+# if(a>=b):
+#     print(f"{a} > ={b} is True")
+# if(a==b):
+#     print(f"{a} == {b} is True")
+
+
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# if(a<b):
+#     print(f"{a} is less than {b}")
+# else:
+#     print(f"{a} is greater than {b}")
+
+
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# if(a<b):print(f"{a} is less than {b}")
+# elif(a>b):print(f"{a} is greater than {b}")
+# elif(a<=b):print(f"{a} is less than or equal to {b}")
+# elif(a>=b):print(f"{a} is greater than or equal to {b}")
+# elif(a==b):print(f"{a} is same as {b}")
+# elif(a!=b):print(f"{a} is not equal to {b}")
+
+# n=int(input("Enter any Integer Value:"))
+# if(n%2==0):
+#     print(f"{n} is an Even Number")
+# else:
+#     print(f"{n} is an Odd Number")
